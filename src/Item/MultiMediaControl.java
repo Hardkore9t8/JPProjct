555b9810a6d226d5b1eae3e2a9a275792206ed92
nash3799 <nash3799@eagle.fgcu.edu>
2018-10-01 10:01:58 -0400

    JP Project v.2

---
+++ src/Item/MultiMediaControl.java
@@ -0,0 +1,17 @@
+
+/**Naldo Ash
+ *This interface creates an interface called MultimediaControl
+ * that will define the following methods.
+public void play();
+public void stop();
+public void previous();
+public void next();*/
+package Item;
+
+public interface MultiMediaControl {
+  void play();
+  void stop();
+  void previous();
+  void next();
+
+}
