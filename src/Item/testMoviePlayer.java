8c41a00d0ee79ef722bd75f43a5525af6c84c71e
nash3799 <nash3799@eagle.fgcu.edu>
2018-10-16 00:11:10 -0400

    JP Project v.2

--- src/Item/testMoviePlayer.java
+++ src/Item/testMoviePlayer.java
@@ -4,7 +4,7 @@ public class testMoviePlayer {
     public static void main(String[] args) {
         MoviePlayer mov = new MoviePlayer("LG" );
         System.out.println(mov.toString());
-
+//
         mov.next();
         mov.play();
         mov.previous();
